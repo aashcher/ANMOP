b338c877cb86a6f3f510c511906320a2b9a8c96a
aashcher <aashcher@yandex.ru>
2024-10-21 18:27:40 +0300

    ela

--- code/PhC2D_empty_lattice.py
+++ code/PhC2D_empty_lattice.py
@@ -34,60 +34,60 @@ def plot_empty_lattice_dispersion():
 
     fig, (ax_sqr, ax_hex) = plt.subplots(1, 2)
 
-    ax_sqr.plot([0, 0], [0, 5], color="grey", linestyle='dashed')
-    ax_sqr.plot([0.5*K_sqr, 0.5*K_sqr], [0, 5], color="grey", linestyle='dashed')
+    ax_sqr.plot([0, 0], [0, 5], color="grey", linestyle='dashed') # Gamma-point
+    ax_sqr.plot([0.5*K_sqr, 0.5*K_sqr], [0, 5], color="grey", linestyle='dashed') # X-point
 
-    ax_hex.plot([0, 0], [0, 5], color="grey", linestyle='dashed')
-    ax_hex.plot([0.5*K_hex, 0.5*K_hex], [0, 5], color="grey", linestyle='dashed')
+    ax_hex.plot([0, 0], [0, 5], color="grey", linestyle='dashed') # Gamma-point
+    ax_hex.plot([K_hex/np.sqrt(3), K_hex/np.sqrt(3)], [0, 5], color="grey", linestyle='dashed') # K-point
 
-    for m1 in range(-2,2):
-        for m2 in range(-2,2):
-              # square MG line
+    for m1 in range(-3,3):
+        for m2 in range(-3,3):
+            # square MG line
             x = np.linspace(0, 0.5*np.sqrt(2)*K_sqr, nump) - 0.5*np.sqrt(2)*K_sqr
             y = cw * dispersion(np.linspace(0.5*K_sqr, 0, nump), 
                                 np.linspace(0.5*K_sqr, 0, nump), 
                                 m1, m2, K1_sqr, K2_sqr)
             ax_sqr.plot(x, y, color="blue")
-              # square GX line
+            # square GX line
             x = np.linspace(0, 0.5*K_sqr, nump)
             y = cw * dispersion(np.linspace(0, 0.5*K_sqr, nump), 
                                 0, m1, m2, K1_sqr, K2_sqr)
             ax_sqr.plot(x, y, color="blue")
-              # square XM line
+            # square XM line
             x = 0.5*K_sqr + np.linspace(0, 0.5*K_sqr, nump)
             y = cw * dispersion(0.5*K_sqr, 
                                 np.linspace(0, 0.5*K_sqr, nump), 
                                 m1, m2, K1_sqr, K2_sqr)
             ax_sqr.plot(x, y, color="blue")
             
-              # hexagonal MG line
-            x = np.linspace(-0.25*np.sqrt(3)*K_hex, 0, nump)
-            y = cw * dispersion(np.linspace(0.375*K_hex, 0, nump), 
-                                np.linspace(0.125*np.sqrt(3)*K_hex, 0, nump), 
+            # hexagonal MG line
+            x = np.linspace(-0.5*K_hex, 0, nump)
+            y = cw * dispersion(np.linspace(0.5*K_hex, 0, nump), 
+                                0,
                                 m1, m2, K1_hex, K2_hex)
             ax_hex.plot(x, y, color="blue")
-              # hexagonal GK line
-            x = np.linspace(0, 0.5*K_hex, nump)
+            # hexagonal GK line
+            x = np.linspace(0, K_hex/np.sqrt(3), nump)
             y = cw * dispersion(np.linspace(0, 0.5*K_hex, nump), 
-                                0, 
+                                np.linspace(0, 0.5*K_hex/np.sqrt(3), nump),
                                 m1, m2, K1_hex, K2_hex)
             ax_hex.plot(x, y, color="blue")
-              # hexagonal KM line
-            x = np.linspace(0, 0.25*K_hex, nump) + 0.5*K_hex
-            y = cw * dispersion(np.linspace(0.5*K_hex, 0.375*K_hex, nump), 
-                                np.linspace(0, 0.125*np.sqrt(3)*K_hex, nump), 
+            # hexagonal KM line
+            x = np.linspace(0, 0.5*K_hex/np.sqrt(3), nump) + K_hex/np.sqrt(3)
+            y = cw * dispersion(0.5*K_hex, 
+                                np.linspace(0.5*K_hex/np.sqrt(3), 0, nump), 
                                 m1, m2, K1_hex, K2_hex)
             ax_hex.plot(x, y, color="blue")
-            
+
     ax_sqr.set_ylabel(r'$\omega\Lambda/2\pi c$')
     ax_sqr.set_xlim([-0.5*np.sqrt(2)*K_sqr, K_sqr])
     ax_sqr.set_ylim([0, 2])
     ax_sqr.set_xticks([0, -0.5*np.sqrt(2)*K_sqr, 0.5*K_sqr, K_sqr], [r'$\Gamma$', 'M', 'X', 'M'])
     ax_sqr.set_title('square lattice', pad = 20)
 
-    ax_hex.set_xlim([-0.25*np.sqrt(3)*K_hex, 0.75*K_hex])
+    ax_hex.set_xlim([-0.5*K_hex, 1.5*K_hex/np.sqrt(3)])
     ax_hex.set_ylim([0, 2])
-    ax_hex.set_xticks([0, -0.25*np.sqrt(3)*K_hex, 0.5*K_hex, 0.75*K_hex], [r'$\Gamma$', 'M', 'K', 'M'])
+    ax_hex.set_xticks([0, -0.5*K_hex, K_hex/np.sqrt(3), 1.5*K_hex/np.sqrt(3)], [r'$\Gamma$', 'M', 'K', 'M'])
     ax_hex.set_yticks([])
     ax_hex.set_title('hexagonal lattice', pad = 20)
 
